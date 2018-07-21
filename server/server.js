73a45e3139ab2d0ca71d79ae3f978475ba08df7e
VjacheslavZ <slavikov.net@mail.ru>
2018-07-21 19:51:05 +0300

    add Server User Routes

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ mongoose.connect(config.DATABASE);
 
 const { User } = require('./models/user');
 const { Book } = require('./models/book');
+const { auth } = require('./middleware/auth');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -33,6 +34,35 @@ app.get('/api/books', (req, res) => {
         res.send(doc);
     })
 });
+app.get('/api/getReviewer', (req, res) => {
+    let id = req.query.id;
+
+    User.findById(id, (err, doc) => {
+        if(err) return res.status(400).send(err);
+        res.send({
+            name: doc.name,
+            lastName: doc.lastName,
+        })
+    })
+});
+app.get('/api/users', (req, res) => {
+    User.find({}, (err, users) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).send(users)
+    })
+});
+app.get('/api/user_posts', (req, res) => {
+    Book.find({ownerId: req.query.user}).exec((err, docs) => {
+        if(err) return res.status(400).send(err);
+        res.send(docs)
+    })
+});
+app.get('/api/logout', auth, (req, res) => {
+    req.user.deleteToken(req.token, (err, user)=>{
+        if(err) return res.status(400).send(err);
+        res.sendStatus(200);
+    })
+});
 
 /*POST*/
 app.post('/api/book', (req, res) => {
@@ -44,6 +74,39 @@ app.post('/api/book', (req, res) => {
             post: true,
             bookId: doc._id
         })
+    })
+});
+app.post('/api/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false});
+        res.status(200).json({
+            success: true,
+            user: doc
+        })
+    })
+});
+app.post('/api/login', (req, res) => {
+    User.findOne({'email':req.body.email}, (err, user) =>{
+        if(!user) return res.json({isAuth:false, message: 'Auth failed, email not found'});
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({
+                isAuth:false,
+                message:'Wrong password',
+            });
+
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+
+                res.cookie('auth', user.token).json({
+                    isAuth: true,
+                    id: user._id,
+                    email: user.email
+                })
+            })
+        });
     })
 });
 

841d7badcc939f4865f10820d5bebd9182dbd325
VjacheslavZ <slavikov.net@mail.ru>
2018-07-22 21:25:57 +0300

    add action getBookWidthReviewer, add component BookView

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export function getBooks(limit = 10, start = 0, order = 'asc', list=''){
+export function getBooks(limit = 10, start = 0, order = 'asc', list='') {
     const request = axios.get(`/api/books?limit=${limit}&skip=${start}&order=${order}`)
                          .then((response) => {
                             if(list) {
@@ -12,5 +12,37 @@ export function getBooks(limit = 10, start = 0, order = 'asc', list=''){
     return {
         type: 'GET_BOOKS',
         payload: request
+    }
+}
+
+export function getBookWidthReviewer(id) {
+    const request = axios.get(`/api/getBook?id=${id}`);
+
+    return (dispatch) => {
+        request.then(({data}) => {
+            let book = data;
+
+            axios.get(`/api/getReviewer?id=${book.ownerId}`)
+            .then(({data}) => {
+                let response = {
+                    book,
+                    reviewer: data
+                };
+                dispatch({
+                    type: 'GET_BOOK_W_REVIEWER',
+                    payload: response
+                })
+            });
+        })
+    }
+}
+
+export function clearBookWidthReviewer() {
+    return {
+        type: 'CLEAR_BOOK_W_REVIEWER',
+        payload: {
+            book: {},
+            reviewer: {}
+        }
     }
 }

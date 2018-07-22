d1b92da17e38bb2f0283d35a05112990385d0064
VjacheslavZ <slavikov.net@mail.ru>
2018-07-22 13:07:59 +0300

    add sideNav, header, hoc-layout

---
+++ client/src/components/Header/Sidenav/sidenav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import SideNav from 'react-simple-sidenav';
+
+const Nav = (props) => {
+    return (
+        <SideNav
+            showNav={props.showNav}
+            onHideNav={props.onHideNav}
+            navStyle={{
+                background: '#242424',
+                maxWidth: "200px",
+            }}
+        >
+            items
+        </SideNav>
+    );
+};
+
+export default Nav;
